8e1ea6fdd7d608101bcc843162004fc425202d54
Zili Zhang <zzhang@gorillagroup.com>
2018-11-19 11:40:39 -0500

    add task component

---
+++ src/app/classes/tick.ts
@@ -0,0 +1,7 @@
+import { DateTime } from 'luxon';
+
+export class Tick {
+    tickInAt: DateTime;
+    tockOutAt: DateTime;
+    duration: number;
+}
---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { DateTime, Duration } from 'luxon';
+
+import { Tick } from '../../classes/tick';
+
+@Component({
+  selector: 'c-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.scss']
+})
+export class TaskComponent implements OnInit {
+    now: DateTime = DateTime.local();
+    ticks: Tick[] = [];
+    timer: any;
+    totalDuration: number = 0;
+    active: boolean = false;
+
+
+    constructor() { }
+
+    ngOnInit(): void {
+        setInterval(()=> {
+            this.now = DateTime.local();
+        }, 60000);
+    }
+
+    track(event: MouseEvent): void {
+        event.preventDefault();
+        if(this.active) {
+            this.active = false;
+            let lastTick = this.ticks[this.ticks.length - 1];
+            lastTick.tockOutAt = this.now;
+            lastTick.duration  = lastTick.tockOutAt.diff(lastTick.tickInAt, 'hours').hours - 0;
+            this.totalDuration += lastTick.duration;
+            this.endTimer();
+        } else {
+            this.active = true;
+            let tick = new Tick;
+            tick.tickInAt = this.now;
+            this.ticks.push(tick);
+            this.startTimer(tick);
+        }
+    }
+
+    startTimer(tick: Tick): void {
+        this.timer = this.now.diff(tick.tickInAt, ['hours','minutes']);
+        setInterval(()=> {
+            this.timer = this.now.diff(tick.tickInAt, ['hours','minutes']);
+            console.log(this.timer.minutes);
+        }, 60000);
+    }
+
+    endTimer(): void {
+        this.timer = null;
+    }
+
+}
