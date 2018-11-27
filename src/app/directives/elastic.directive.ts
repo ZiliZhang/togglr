6ef9e9e0b18078b5542ac3a46764bc103ecdb2ef
Zili Zhang <zili@oceana.im>
2018-11-26 23:34:17 -0500

    fix auto-shrink of elastic textarea

--- src/app/directives/elastic.directive.ts
+++ src/app/directives/elastic.directive.ts
@@ -11,11 +11,12 @@ export class ElasticDirective {
 
     constructor(public element: ElementRef){}
 
-    ngAfterContentChecked(): void{
+    ngAfterContentChecked(): void {
         this.adjust();
     }
     adjust(): void{
-        this.element.nativeElement.style.overflow = 'hidden';
+        this.element.nativeElement.style.overflowY = 'hidden';
+        this.element.nativeElement.style.height = 'auto';
         this.element.nativeElement.style.height = this.element.nativeElement.scrollHeight + "px";
     }
 
